14218cd167191be7e849703a0104457fa00028fe
Sizon <sizon@vidrada.org>
2020-04-14 13:40:13 +0300

    fixed modal scroll

--- script/main.js
+++ script/main.js
@@ -1,15 +1,18 @@
 new WOW().init();
 
-const cartButton = document.getElementById('cart-button'),
+const body = document.querySelector('body'),
   modal = document.querySelector('.modal'),
+  cartButton = document.getElementById('cart-button'),
   close = document.querySelector('.close'),
   cancel = document.getElementById('cancel');
 
 // Function
-const toggleModal = () => modal.classList.toggle('modal-open');
+const toggleModal = () => {
+  modal.classList.toggle('modal-open');
+  body.classList.toggle('body-hidden');
+};
 
 // Call
-
 cartButton.addEventListener('click', toggleModal);
 close.addEventListener('click', toggleModal);
 cancel.addEventListener('click', toggleModal);
